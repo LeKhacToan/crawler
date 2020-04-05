518d4c84418aaad616f6c4f01e58af8cd30f8a4b
bonboniee <lekhactoan1898@gmail.com>
2020-04-05 21:23:17 +0700

    remove: download file

--- downloadImage.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-const https = require("https");
-
-export const saveImageToDisk = (url, localPath) => {
-  let file = fs.createWriteStream(localPath);
-  https.get(url, function (response) {
-    response.pipe(file);
-  });
-};
